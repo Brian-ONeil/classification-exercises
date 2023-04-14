40c57fcb81ecbe25aeee360460ce291d64ee0553
Brian-ONeil <oneilbrianlee@gmail.com>
2023-04-14 16:59:05 -0500

    updating files

--- acquire.py
+++ acquire.py
@@ -1,20 +1,120 @@
-def get_titanic_data(SQL_query, directory, filename="titanic.csv"):
+#######IMPORTS
+
+import pandas as pd
+import os
+import env
+
+
+
+#######FUNCTIONS
+
+
+
+
+
+def new_titanic_data(SQL_query):
+    """
+    This function will:
+    - take in a SQL_query
+    - create a db_url to mySQL
+    - return a df of the given query from the titanic_db
+    """
+    url = env.get_db_url('titanic_db')
+    
+    return pd.read_sql(SQL_query, url)
+
+
+
+
+def get_titanic_data(SQL_query, filename = 'titanic.csv'):
     """
     This function will:
     - Check local directory for csv file
         - return if exists
-    - If csv doesn't exists:
-        - create a df of the SQL_query
-        - write df to csv
-    - Output titanic df
-"""
-    if os.path.exists(directory + filename):
-        df = pd.read_csv(filename) 
+    - if csv doesn't exist:
+        - creates df of sql query
+        - writes df to csv
+    - outputs titanic df
+    """
+    if os.path.exists(filename): 
+        df = pd.read_csv(filename)
         return df
-    
     else:
         df = new_titanic_data(SQL_query)
-        
-        #want to save to csv
+
         df.to_csv(filename)
         return df
+    
+    
+    
+    
+    
+def new_iris_data(SQL_query):
+    """
+    This function will:
+    - take in a SQL_query
+    - create a db_url to mySQL
+    - return a df of the given query from the iris_db
+    """
+    url = env.get_db_url('iris_db')
+    
+    return pd.read_sql(SQL_query, url)
+
+
+
+def get_iris_data(SQL_query, filename = 'iris.csv'):
+    """
+    This function will:
+    - Check local directory for csv file
+        - return if exists
+    - if csv doesn't exist:
+        - creates df of sql query
+        - writes df to csv: defaulted to iris.csv
+    - outputs iris df
+    """
+    if os.path.exists(filename): 
+        df = pd.read_csv(filename)
+        return df
+    else:
+        df = new_iris_data(SQL_query)
+
+        df.to_csv(filename)
+        return df
+    
+    
+
+    
+    
+def new_telco_data(SQL_query):
+    """
+    This function will:
+    - take in a SQL_query
+    - create a db_url to mySQL
+    - return a df of the given query from the telco_db
+    """
+    url = env.get_db_url('telco_churn')
+    
+    return pd.read_sql(SQL_query, url)
+
+
+
+
+def get_telco_data(SQL_query, filename = 'telco.csv'):
+    """
+    This function will:
+    - Check local directory for csv file
+        - return if exists
+    - if csv doesn't exist:
+        - creates df of sql query
+        - writes df to csv: defaulted to telco.csv
+    - outputs iris df
+    """
+    if os.path.exists(filename): 
+        df = pd.read_csv(filename)
+        return df
+    else:
+        df = new_telco_data(SQL_query)
+
+        df.to_csv(filename)
+        return df
+    
---
+++ prepare.py
@@ -0,0 +1,60 @@
+#######IMPORTS
+
+import pandas as pd
+import os
+import env
+from sklearn.model_selection import train_test_split
+
+
+#######FUNCTIONS
+
+
+def clean_iris(iris_df):
+    
+    iris_df = iris_df.drop(axis=1, columns=['species_id'])
+    
+    iris_df = iris_df.rename(columns = {'species_name': 'species'})
+    
+    dummy_df = pd.get_dummies((iris_df.species), drop_first=True)
+    
+    iris_df = pd.concat([iris_df, dummy_df], axis=1)
+    return iris_df
+
+def prep_titanic(titanic_df):
+    '''
+    This function will clean the the titanic dataset
+    '''
+    titanic_df = titanic_df.drop("Unnamed: 0" ,axis=1)
+    
+    titanic_df = titanic_df.drop(columns =['embark_town','class','age','deck'])
+
+    titanic_df.embarked = titanic_df.embarked.fillna(value='S')
+
+    dummy_df = pd.get_dummies(titanic_df[['sex','embarked']], drop_first=True)
+    titanic_df = pd.concat([titanic_df, dummy_df], axis=1)
+    return titanic_df
+
+def prep_telco(telco_df):
+    '''
+    This function will clean the the telco dataset
+    '''
+    telco_df = telco_df.drop("Unnamed: 0" ,axis=1)
+
+    dummy_df = pd.get_dummies((telco_df.gender), drop_first=True)
+    telco_df = pd.concat([telco_df, dummy_df], axis=1)
+    return telco_df
+
+def split_data(df, stratify_col):
+    '''
+    Takes in two arguments the dataframe name and the ("name" - must be in string format) to stratify  and
+    return train, validate, test subset dataframes will output train, validate, and test in that order
+    '''
+    train, test = train_test_split(df, #first split
+                                   test_size=.2,
+                                   random_state=123,
+                                   stratify=df[stratify_col])
+    train, validate = train_test_split(train, #second split
+                                    test_size=.25,
+                                    random_state=123,
+                                    stratify=train[stratify_col])
+    return train, validate, test
